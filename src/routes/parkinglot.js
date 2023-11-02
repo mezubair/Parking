847cb77f245578882549eae0f65e66177d5c3e7c
Muneeb Ul Rehmaan <muneebulrehman7@gmail.com>
2023-11-02 22:01:18 +0530

    api added

---
+++ src/routes/parkinglot.js
@@ -0,0 +1,103 @@
+let parkingLotsData = [
+    {
+        id: 1,
+        name: "Downtown Parking",
+        latitude: 78.90,
+        longitude: 123.456,
+        address: "123 Example Street, Srinagar",
+        locality: "Locality 1",
+        city: "Srinagar",
+        chargesPerHour: 5,
+        contact: {
+            phone: "123-456-7890",
+            email: "downtownparking@example.com"
+        }
+    },
+    {
+        id: 2,
+        name: "Downtown Parking",
+        latitude: 34.071750,
+        longitude: 74.804320,
+        address: "456 Example Street, Srinagar",
+        locality: "Batamaloo",
+        city: "Srinagar",
+        chargesPerHour: 10,
+        contact: {
+            phone: "123-456-7890",
+            email: "downtownparking@example.com"
+        }
+    },
+    {
+        id: 3,
+        name: "Downtown Parking",
+        latitude: 34.071750,
+        longitude: 74.804320,
+        address: "789 Example Street, Srinagar",
+        locality: "Batamaloo",
+        city: "Srinagar",
+        chargesPerHour: 10,
+        contact: {
+            phone: "123-456-7890",
+            email: "downtownparking@example.com"
+        }
+    },
+    {
+        id: 4,
+        name: "Downtown Parking",
+        latitude: 34.071750,
+        longitude: 74.804320,
+        address: "101 Example Street, Srinagar",
+        locality: "Batamaloo",
+        city: "Srinagar",
+        chargesPerHour: 5,
+        contact: {
+            phone: "123-456-7890",
+            email: "downtownparking@example.com"
+        }
+    },
+    {
+        id: 5,
+        name: "Downtown Parking",
+        latitude: 78.90,
+        longitude: 123.456,
+        address: "123 Example Street, Srinagar",
+        locality: "Iqbal Park",
+        city: "Srinagar",
+        chargesPerHour: 5,
+        contact: {
+            phone: "123-456-7890",
+            email: "downtownparking@example.com"
+        }
+    },
+    {
+        id: 6,
+        name: "Downtown Parking",
+        latitude: 78.90,
+        longitude: 123.456,
+        address: "456 Example Street, Srinagar",
+        locality: "Iqbal Park",
+        city: "Srinagar",
+        chargesPerHour: 5,
+        contact: {
+            phone: "123-456-7890",
+            email: "downtownparking@example.com"
+        }
+    },
+    {
+        id: 7,
+        name: "Downtown Parking",
+        latitude: 78.90,
+        longitude: 123.456,
+        address: "789 Example Street, Srinagar",
+        locality: "Iqbal Park",
+        city: "Srinagar",
+        chargesPerHour: 5,
+        contact: {
+            phone: "123-456-7890",
+            email: "downtownparking@example.com"
+        }
+    }
+    // Add more entries here
+];
+
+module.exports = parkingLotsData;
